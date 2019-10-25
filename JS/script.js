ea0dd5afbeb9a5341733a470b758c320643f32c8
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-10-25 12:37:07 -0500

    Clock UI is setup and behavior for modifying session and break time is implemented.

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,34 @@
 (function () {
     let clock = new Clock();
     clock.displayCurrentTime();
+    clock.displaySessionTime();
+    clock.displayBreakTime();
+    clock.displaySessionCount();
+
+    // Event Listeners
+    $(".time-session .plus").click(function() {
+        clock.changeSessionTime("add");
+    });
+    $(".time-session .minus").click(function() {
+        clock.changeSessionTime("subtract");
+    });
+    $(".time-break .plus").click(function() {
+        clock.changeBreakTime("add");
+    });
+    $(".time-break .minus").click(function() {
+        clock.changeBreakTime("subtract");
+    });
+    
 })();
 
 function Clock() {
-    var startTime = 1500; // Starting a value for our timer
-    var currentTime = 1500; // Current time for our timer 
-    var sessionTime = 2400; // Length of a break in seconds
-    var breakTime = 800; // Length of a break in seconds 
-    var sessionCount = 0; // The number of sessions
-    var mode = "Session"; // Keeps track of what mode we're in - session or break
+    var startTime = 1500, // Starting a value for our timer
+        currentTime = 1500, // Current time for our timer 
+        sessionTime = 2400, // Length of a break in seconds
+        breakTime = 800, // Length of a break in seconds 
+        sessionCount = 0, // The number of sessions
+        mode = "Session", // Keeps track of what mode we're in - session or break
+        active = false; // Keeps track of whether the clock is running or not
 
     // Function to convert a number of seconds into a formatted time string 
     function formatTime(secs) {
@@ -39,9 +58,9 @@ function Clock() {
         return result;
     }
 
-    // Function to display the startTime
+    // Function to display the time remaining on the timer
     this.displayCurrentTime = function() {
-        $(".main-display").text(formatTime(startTime));
+        $(".main-display").text(formatTime(currentTime));
     }
 
     // Function to display the sessionTime 
@@ -50,7 +69,7 @@ function Clock() {
     }
 
     // Function to display the breakTime 
-    this.displayBreakTime = function () {
+    this.displayBreakTime = function() {
         $(".time-break-display").text(parseInt(breakTime / 60) + " min");
     }
 
@@ -59,12 +78,45 @@ function Clock() {
         if (sessionCount === 0) {
             // If our session count is 0, we should show the text Pomodoro Clock
             $(".session-count").html("<h2>Pomodoro Clock</h2>");
-        } else if (type === "Session") {
+        } else if (mode === "Session") {
             // If our session count is greater than 0 and we're in a session, we should show which session we're in 
             $(".session-count").html("<h2>Session " + sessionCount + "</h2>");
         } else if (mode === "Break") {
             // If we're in a break, we should show the text break
             $(".session-count").html("<h2>Break!</h2");
+        }
+    }
+
+    // CHANGE TIME FUNCTIONS
+
+    // Function to add or subtract 60 seconds from the session time whenever the plus or minus buttons are interacted with.
+    this.changeSessionTime = function(command) {
+        if (!active) {
+            if (command === "add") {
+                // Add a minute to our session time
+                sessionTime += 60;
+            } else if (sessionTime > 60) {
+                // If session time is greater than 1 minute, subtract a minute from it.
+                sessionTime -= 60;
+            }
+            currentTime = sessionTime;
+            startTime = sessionTime;
+            this.displaySessionTime();
+            this.displayCurrentTime();
+        }
+
+    }
+
+    this.changeBreakTime = function(command) {
+        if (!active) {
+            if (command === "add") {
+                // Add a minute to our session time
+                breakTime += 60;
+            } else if (breakTime > 60) {
+                // If session time is greater than 1 minute, subtract a minute from it.
+                breakTime -= 60;
+            }
+            this.displayBreakTime();
         }
     }
 }
